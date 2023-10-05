a04bb66e9066ca8fa76b31f210d6b9288a553784
Micado Pierre <Biml16gb@gmail.com>
2023-10-05 16:55:08 -0400

    First Objects

---
+++ index.js
@@ -0,0 +1,25 @@
+const person = { 
+    firstName: "Todd",
+    age: 28
+}
+// Print the person's name
+console.log(person.firstName) 
+
+// add one to the person's age
+person.age++
+
+console.log(person.age)
+
+person.lastName = "Albert"  // Add a new property to the object
+/*
+const person = {
+    firstName = "Todd"
+    age = 29
+    lastName: "Albert"
+
+}
+*/
+
+console.log(person.lastName)
+
+
